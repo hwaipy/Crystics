9dddcd5b81cc72f280324c821a90e2ce5fed96b8
Hwaipy Li <hwaipy@gmail.com>
2015-03-08 20:36:07 +0800

    Try-LuaJ
    It shows that LuaJ can work, and can calculate simple formulas.

--- Crystics/src/main/java/com/hwaipy/crystics/TryLuaJ.java
+++ Crystics/src/main/java/com/hwaipy/crystics/TryLuaJ.java
@@ -5,10 +5,23 @@
  */
 package com.hwaipy.crystics;
 
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.JsePlatform;
+
 /**
  *
  * @author Hwaipy
  */
 public class TryLuaJ {
-  
+
+  public static void main(String[] args) {
+    Globals globals = JsePlatform.standardGlobals();
+    LuaValue chunk = globals.load("a=123*123");
+    for (int i = 0; i < 10000000; i++) {
+//      chunk.call();
+      int a = 123 * 123;
+    }
+  }
+
 }
